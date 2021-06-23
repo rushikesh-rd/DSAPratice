1d678c0558642d0925ae69893bd6b6fedd76f396
rushikesh-rd <mailtorushikesh1111@gmail.com>
2021-06-23 08:33:40 +0530

    leetcode commit

---
+++ GoodTriplets.java
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/count-good-triplets/submissions/
+class GoodTriplets {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int ans = 0;
+        for(int i=0 ; i<arr.length-2 ; i++) {
+            for(int j=i+1 ; j<arr.length-1 ; j++) {
+                if(Math.abs(arr[i] - arr[j]) <= a ) {
+                    for(int k=j+1 ; k<arr.length ; k++) {
+                        if(Math.abs(arr[j] - arr[k]) <= b) {
+                            if(Math.abs(arr[i] - arr[k]) <= c) {
+                                ans++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
